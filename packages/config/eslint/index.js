35f1a725104140303567049ccb21bcfc1b4c4e2e
yun jong yun <yunyami0605@gmail.com>
2025-07-08 17:49:04 +0900

    chore: 프로젝트 모노레포 기본 셋팅
    1. ai-diary 프로젝트 next 셋업
    2. ai-img-shop 프로젝트 next 셋업
    3. prettier, eslint 셋업
    4. 모노레포 폴더 구조 및 환경 셋업

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,35 @@
+// yarn add -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true, // <-- 핵심!
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "plugin:react-hooks/recommended",
+    "prettier", // ✅ 마지막에 추가 (필수)
+  ],
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  rules: {
+    camelcase: ["error", { properties: "always" }],
+    "react/jsx-pascal-case": "error",
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "react/react-in-jsx-scope": "off", // Next.js는 필요 없음
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
