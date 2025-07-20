8b334638be3ba71e1b5f64b28e988f5c728bc351
yun jong yun <yunyami0605@gmail.com>
2025-07-20 22:53:31 +0900

    [@my/ui] refactor: checkbox 컴포넌트 수정
    1. icon으로 체크 on/off 하도록 수정

--- packages/ui/src/base/Checkbox.tsx
+++ packages/ui/src/base/Checkbox.tsx
@@ -3,6 +3,7 @@
 import { InputHTMLAttributes, forwardRef } from "react";
 import clsx from "clsx";
 import styles from "./Checkbox.module.scss";
+import { FaRegCheckCircle, FaRegCircle } from "react-icons/fa";
 
 type CheckboxProps = Omit<InputHTMLAttributes<HTMLInputElement>, "type"> & {
   error?: string;
@@ -12,14 +13,20 @@ type CheckboxProps = Omit<InputHTMLAttributes<HTMLInputElement>, "type"> & {
  * @description 공통 체크박스 컴포넌트
  */
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ className, error, ...props }, ref) => {
+  ({ className, error, checked, ...props }, ref) => {
     return (
-      <input
-        ref={ref}
-        type="checkbox"
-        {...props}
-        className={clsx(styles.checkbox, error && styles.error, className)}
-      />
+      <label className={clsx(styles.wrapper, className)}>
+        <input
+          ref={ref}
+          type="checkbox"
+          {...props}
+          className={clsx(styles.checkbox, error && styles.error, className)}
+        />
+
+        <span className={clsx(styles.icon, error && styles.error)}>
+          {checked ? <FaRegCheckCircle fill={"#0094ff"} /> : <FaRegCheckCircle fill={"#9ea1a8"} />}
+        </span>
+      </label>
     );
   },
 );
