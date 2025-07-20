89d4b61f8a7ee26c8bd456f10709d52494370e74
yun jong yun <yunyami0605@gmail.com>
2025-07-21 00:26:05 +0900

    [ai-hellofit] auth social form 컴포넌트 추가
    1. social login form 컴포넌트 ui 추가

---
+++ apps/ai-hellofit-frontend/src/features/auth/_components/form/SocialLoginForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import styles from "./SocialLoginForm.module.scss";
+import React, { useState } from "react";
+import KakaoRectButton from "../button/KakaoRectButton";
+import AppleRectButton from "../button/AppleRectButton";
+import TextButton from "@/shared/button/TextButton";
+import { useRouter } from "next/navigation";
+
+/**
+ *@description 소셜 로그인 버튼들
+ */
+function SocialLoginForm() {
+  const router = useRouter();
+
+  return (
+    <section className={styles.wrapper}>
+      <KakaoRectButton
+        className={styles.kakao}
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+
+      <AppleRectButton
+        className={styles.apple}
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+
+      <TextButton
+        className={styles.other}
+        name={"다른 방법으로 로그인"}
+        onClick={() => router.push("/login")}
+      />
+    </section>
+  );
+}
+
+export default SocialLoginForm;
