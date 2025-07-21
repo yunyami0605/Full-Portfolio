9f12f86dd61284247ac4435cdf0f18ede11c1e47
yunjongyun <yunyami0605@gmail.com>
2025-07-21 12:50:09 +0900

    [ai-hellofit] style: global css 변경
    1. 글자태그 color 추가
    2. 확장자 변경 scss

--- apps/ai-hellofit-frontend/src/app/layout.tsx
+++ apps/ai-hellofit-frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import { BaseLayout } from "@my/ui";
 
 const geistSans = Geist({
