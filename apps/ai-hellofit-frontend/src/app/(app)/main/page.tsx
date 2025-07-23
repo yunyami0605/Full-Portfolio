d1c5c7c7234d92cbb12040f81ec4c0995fc83ff2
yunjongyun <yunyami0605@gmail.com>
2025-07-23 13:28:27 +0900

    [ai-hellofit] 메인페이지 추가
    2. 메인페이지ui추가

--- apps/ai-hellofit-frontend/src/app/(app)/main/page.tsx
+++ apps/ai-hellofit-frontend/src/app/(app)/main/page.tsx
@@ -1,10 +1,26 @@
 "use client";
 
-import styles from "./page.module.scss";
+import styles from "./MainPage.module.scss";
 import React from "react";
+import RecommendationInfoCard from "@/features/recommendation/_components/RecommendationInfoCard";
+import Header from "@/shared/layout/Header";
+import { FaRegBell } from "react-icons/fa";
+import IconButton from "@/shared/button/IconButton";
 
 function MainPage() {
-  return <section className={styles.page_layout}>MainPage</section>;
+  return (
+    <section className={styles.page_layout}>
+      <Header left={"헬로핏"} right={<IconButton icon={FaRegBell} />} />
+
+      <section className={styles.inner_wrapper}>
+        <RecommendationInfoCard title="오늘의 추천 식단 변경하기" />
+
+        <div className={styles.ads_wrapper}>광고</div>
+
+        <RecommendationInfoCard title="오늘의 추천 운동 변경하기" />
+      </section>
+    </section>
+  );
 }
 
 export default MainPage;
