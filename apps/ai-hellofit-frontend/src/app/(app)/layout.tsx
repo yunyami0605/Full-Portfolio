cf36431927ea5daab40d6c2f6f32365377fd1482
yunjongyun <yunyami0605@gmail.com>
2025-07-23 13:25:50 +0900

    [ai-hellofit] refactor: page 추가에 따른 헤더 변경
    1. 유저정보 등록 페이지 헤더 값 추가

--- apps/ai-hellofit-frontend/src/app/(app)/layout.tsx
+++ apps/ai-hellofit-frontend/src/app/(app)/layout.tsx
@@ -8,8 +8,12 @@ const headerMap = {
   "/signup": { title: "회원가입", back: true },
   "/privacy": { title: "개인정보처리방침", back: true },
   "/main": { title: "", back: false, noHeader: true },
+  "/user/register": { title: "", back: false, noHeader: true },
 };
 
+/**
+ *@description app 기본 레이아웃
+ */
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const headerProps = headerMap[pathname as keyof typeof headerMap] ?? { title: "" };
