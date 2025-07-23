eee5c17ce3f2a74f7eb31079809e23886c24dd6b
yunjongyun <yunyami0605@gmail.com>
2025-07-23 15:35:50 +0900

    [ai-hellofit] feat: 음식/운동 추천 페이지 ui 추가
    1. 카드선택버튼

---
+++ apps/ai-hellofit-frontend/src/app/(app)/recommendation/page.tsx
@@ -0,0 +1,36 @@
+import styles from "./Recommendation.module.scss";
+import React from "react";
+import Card from "@/shared/layout/Card";
+import ChoiceRecommendationCardButton from "@/features/recommendation/_components/ChoiceRecommendationCardButton";
+import { Center, Text } from "@my/ui";
+import ActiveButton from "@/shared/button/ActiveButton";
+
+function RecommendationPage() {
+  const arr = Array.from({ length: 4 }, (_, i) => ({
+    title: `test${i + 1}`,
+    data: [1, 2, 3],
+  }));
+  return (
+    <section className={styles.page_layout}>
+      <Card className={styles.card_wrapper}>
+        <Center className={styles.card_time}>
+          <Text>아침</Text>
+        </Center>
+
+        {arr.map((item, i) => (
+          <React.Fragment key={i}>
+            <ChoiceRecommendationCardButton
+              title={item.title}
+              data={item.data}
+              isChecked={i === 0}
+            />
+          </React.Fragment>
+        ))}
+
+        <ActiveButton name={"다음"} />
+      </Card>
+    </section>
+  );
+}
+
+export default RecommendationPage;
