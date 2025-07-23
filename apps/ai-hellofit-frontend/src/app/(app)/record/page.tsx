5aad5ecfe8b60f82e9ee192a1d5e0c39a44a052c
yunjongyun <yunyami0605@gmail.com>
2025-07-23 17:55:45 +0900

    [ai-hellofit] record 페이지 추가
    1. record 페이지 ui 추가

---
+++ apps/ai-hellofit-frontend/src/app/(app)/record/page.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import styles from "./RecordPage.module.scss";
+import React, { Fragment, useState } from "react";
+import { Button, Row, Text } from "@my/ui";
+import { FaAngleRight } from "react-icons/fa";
+import SmallRoundedButton from "@/shared/button/SmallRoundedButton";
+import RecordCard from "@/features/record/_components/RecordCard";
+
+type Props = {};
+function RecordPage() {
+  const tmp = Array.from({ length: 4 });
+  return (
+    <section className={styles.page_layout}>
+      <section className={styles.top_wrapper}>
+        <Text className={styles.top_wrapper_title}>나의 기록</Text>
+
+        <Button className={styles.calendar_go_button}>
+          <Text>캘린더 보기</Text>
+
+          <FaAngleRight fill={"#333"} size={18} />
+        </Button>
+      </section>
+
+      <Row className={styles.tab_list}>
+        <SmallRoundedButton name={"운동"} isClicked={true} />
+        <SmallRoundedButton name={"식단"} isClicked={false} />
+      </Row>
+
+      {tmp.map((item, i) => (
+        <Fragment key={i}>{<RecordCard />}</Fragment>
+      ))}
+    </section>
+  );
+}
+
+export default RecordPage;
