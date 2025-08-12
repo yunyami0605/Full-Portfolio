929017b24e6c3982c7740c28400bb6db0811caa0
yunjongyun <yunyami0605@gmail.com>
2025-08-12 15:31:59 +0900

    [ai-diary] fix: login schema 수정
    1. login form password schema 조건 추가

--- apps/ai-hellofit-frontend/src/app/schemas/loginSchema.ts
+++ apps/ai-hellofit-frontend/src/app/schemas/loginSchema.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email("유효한 이메일을 입력해주세요."),
-  password: z.string().min(6, "비밀번호는 최소 6자 이상입니다."),
+  email: z.email("유효한 이메일을 입력해주세요."),
+  password: z
+    .string()
+    .min(8, "비밀번호는 최소 8자 이상입니다.")
+    .max(20, "비밀번호는 최대 20자 이하입니다."),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
