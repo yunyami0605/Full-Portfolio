3e24f86492b70a53823193ecabc402df01478e90
yunjongyun <yunyami0605@gmail.com>
2025-08-22 19:56:39 +0900

    [ai-hellofit] feat: api interceptor 추가
    1. axios interceptor 추가 및 accessToken 넣기

--- apps/ai-hellofit-frontend/src/libs/apiCall.ts
+++ apps/ai-hellofit-frontend/src/libs/apiCall.ts
@@ -1,6 +1,19 @@
+import { useAccessTokenStore } from "@/features/auth/_stores/accessToken.store";
 import axios from "axios";
 
 export const apiCall = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // ex) https://api.example.com
   withCredentials: true, // 쿠키를 쓰면 true
 });
+
+apiCall.interceptors.request.use(
+  (config) => {
+    const token = useAccessTokenStore.getState().accessToken;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
