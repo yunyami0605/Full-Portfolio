1ddb1b8e70572e7f76bac17f530bb8dd5f3d4f0e
yunjongyun <yunyami0605@gmail.com>
2025-08-27 23:48:59 +0900

    [ai-hellofit]  feat: api error 응답 타입 추가
    1.  api error 응답 타입 추가

--- apps/ai-hellofit-frontend/src/shared/types/api.ts
+++ apps/ai-hellofit-frontend/src/shared/types/api.ts
@@ -1,3 +1,13 @@
+import { AxiosError } from "axios";
+
 export type MutationResponse = {
   success: boolean;
 };
+
+export type ErrorResponse = {
+  code: string;
+  message: string;
+  field?: string;
+};
+
+export type AxiosErrorResponse = AxiosError<ErrorResponse>;
