dc890497756ee3eca3bd1610d6fd6ef793f12d10
yunjongyun <yunyami0605@gmail.com>
2025-08-31 21:44:25 +0900

    [ai-hellofit] home page ui 변경
    1. 미사용 ui 제거

--- apps/ai-hellofit-frontend/src/app/page.tsx
+++ apps/ai-hellofit-frontend/src/app/page.tsx
@@ -1,10 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/social");
-  return (
-    <div className="">
-      <div className="bg-orange--10 text-white">버튼</div>
-    </div>
-  );
+  return redirect("/social");
 }
